43b8ac0e12603b983db1818742816e19cfd07845
Toru Choi <toru.choi@11street.my>
2018-12-08 17:22:45 +0800

    initialization of retrofit

--- app/src/main/java/my/toru/jetpacktutorial/model/remote/JetPackNetworkApi.kt
+++ app/src/main/java/my/toru/jetpacktutorial/model/remote/JetPackNetworkApi.kt
@@ -1,7 +1,10 @@
 package my.toru.jetpacktutorial.model.remote
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.QueryMap
 
@@ -12,9 +15,20 @@ interface JetPackNetworkApi {
     companion object {
         val api:JetPackNetworkApi = Retrofit.Builder()
                 .baseUrl("")
-                .addConverterFactory(Gson)
+                .client(OkHttp3Initializer.okHttp3())
+                .addConverterFactory(GsonConverterFactory.create())
                 .build()
                 .create(JetPackNetworkApi::class.java)
     }
 }
+
+object OkHttp3Initializer{
+    private fun initInterceptor():HttpLoggingInterceptor
+            = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+    fun okHttp3():OkHttpClient = OkHttpClient.Builder()
+            .addNetworkInterceptor(initInterceptor())
+            .build()
+}
+
 
