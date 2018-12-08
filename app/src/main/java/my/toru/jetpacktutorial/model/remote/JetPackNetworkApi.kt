d24ff7ed48241295dd0725b29a3727ba1a49a632
Toru Choi <toru.choi@11street.my>
2018-12-08 17:53:21 +0800

    test code for retrofit

--- app/src/main/java/my/toru/jetpacktutorial/model/remote/JetPackNetworkApi.kt
+++ app/src/main/java/my/toru/jetpacktutorial/model/remote/JetPackNetworkApi.kt
@@ -7,14 +7,18 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.QueryMap
+import retrofit2.http.Url
 
 interface JetPackNetworkApi {
     @GET
     fun getData(@QueryMap query:HashMap<String, String>): Call<String>
 
+    @GET
+    fun getFakeData(@Url url:String): Call<String>
+
     companion object {
         val api:JetPackNetworkApi = Retrofit.Builder()
-                .baseUrl("")
+                .baseUrl("https://jsonplaceholder.typicode.com/")
                 .client(OkHttp3Initializer.okHttp3())
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
