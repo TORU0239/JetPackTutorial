8f0d43d18649e07cefac9c77df3fa4ee361149f7
Toru Choi <toru.choi@11street.my>
2018-12-27 16:24:41 +0800

    :

---
+++ app/src/main/java/my/toru/jetpacktutorial/ui/launch/LaunchActivity.kt
@@ -0,0 +1,13 @@
+package my.toru.jetpacktutorial.ui.launch
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import my.toru.jetpacktutorial.R
+
+class LaunchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launch)
+    }
+}
