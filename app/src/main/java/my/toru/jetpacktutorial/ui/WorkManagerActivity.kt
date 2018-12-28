a301ad8ecf3306efa7228d7674f2549c3b8c184a
Toru Choi <toru.choi@11street.my>
2018-12-28 11:00:36 +0800

    workmanager initialization

---
+++ app/src/main/java/my/toru/jetpacktutorial/ui/WorkManagerActivity.kt
@@ -0,0 +1,13 @@
+package my.toru.jetpacktutorial.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import my.toru.jetpacktutorial.R
+
+class WorkManagerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_work_manager)
+    }
+}
